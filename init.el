cc65b4aee91ca38f31316906509c2c525609dc18
Nathan Ensmenger <nathan.ensmenger@gmail.com>
2017-02-16 13:25:47 -0500

    minor tweaks to org-level headers

--- init.el
+++ init.el
@@ -757,7 +757,13 @@ whether the window is selected."
  (setq-default evil-search-module 'isearch)
 (setq evil-ex-interactive-search-highlight nil)
 
+(set-face-attribute 'org-level-1 nil :background 'unspecified :box nil)
+(set-face-attribute 'org-level-2 nil :background 'unspecified :box nil)
+(set-face-attribute 'org-level-3 nil :height 'unspecified :box nil)
 
+;; (set-face-attribute 'org-todo nil )
+(set-face-attribute 'org-done nil :height 0.8 :foreground "dark green")
+                                        ;
 (defun nle-clear ()
        (interactive)
        (set-face-background 'font-lock-comment-face nil)
